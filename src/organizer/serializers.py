a3af4fd5b02d6273b1c8e23f797e22b5d27d6926
Michael Bardouille <michael.bardouille@gmail.com>
2022-06-06 12:41:35 -0400

    feat/Build Serializers

---
+++ src/organizer/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework.serializers import ModelSerializer
+from models import NewsLink, Startup, Tag   
+
+class TagSerializer(ModelSerializer):
+   
+   class Meta:
+       model = Tag
+       fields = "__all__"
+
+class StartupSerializer(ModelSerializer):
+    
+    tags = TagSerializer(many=True)
+
+    class Meta:
+       model = Startup
+       fields = "__all__"
+
+class NewsLinkSerailzer(ModelSerializer):
+
+    startup = StartupSerializer
+
+    class Meta:
+       model = NewsLink
+       fields = "__all__"
---
+++ src/organizer/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import TagApiList, TagApiDetail
+
+urlpatterns = [
+    path("", TagApiList.as_view(), name="api-tag-list"),
+    path(
+        "<int:pk>/", 
+        TagApiDetail.as_view(), 
+        name="api-tag-detail"),
+]
