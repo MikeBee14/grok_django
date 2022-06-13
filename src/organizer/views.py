9a245234de447bd704a10ba66ccafc91a6e6f462
Michael Bardouille <michael.bardouille@gmail.com>
2022-06-13 13:52:29 -0400

    chore/Add generic view classes

--- src/organizer/views.py
+++ src/organizer/views.py
@@ -1,42 +1,31 @@
-from django.shortcuts import (get_list_or_404, get_object_or_404)
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView
+)
 from .models import Startup, Tag
 from .serializers import StartupSerializer, TagSerializer
 
-class TagApiDetail(APIView):
 
-    def get(self, request, pk):
-        tag = get_object_or_404(Tag, pk=pk)
-        s_tag = TagSerializer(tag)
-        return Response(s_tag.data)
+class TagApiDetail(RetrieveAPIView):
 
-
-class TagApiList(APIView):
-
-    def get(self, request):
-        tag_list = get_list_or_404(Tag)
-        s_tag = TagSerializer(tag_list, many=True)
-        return Response(s_tag.data)
-
-class StartupApiDetail(APIView):
-
-    def get(self, request, slug):
-        startup = get_object_or_404(Startup, slug=slug)
-        s_startup = StartupSerializer(
-            startup, context={"request": request}
-        )
-        return Response(s_startup.data)
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    lookup_field = "slug"
 
 
-class StartupApiList(APIView):
+class TagApiList(ListAPIView):
 
-    def get(self, request):
-        startup_list = get_list_or_404(Startup)
-        s_startup = StartupSerializer(
-            startup_list, 
-            many=True,
-            context={"request": request}
-        )
-        return Response(s_startup.data)
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+class StartupApiDetail(RetrieveAPIView):
+
+    queryset = Startup.objects.all()
+    serializer_class = StartupSerializer
+    lookup_field = "slug"
+
+
+class StartupApiList(ListAPIView):
+
+    queryset = Startup.objects.all()
+    serializer_class = StartupSerializer
