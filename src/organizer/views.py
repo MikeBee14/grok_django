d40bba55b5a42270ce8edfbcad5cfabb2449701c
Michael Bardouille <michael.bardouille@gmail.com>
2022-06-17 15:52:41 -0400

    chore/shorten function based views

--- src/organizer/views.py
+++ src/organizer/views.py
@@ -1,6 +1,4 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from django.template import loader
+from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_safe
 from rest_framework.generics import (
     ListAPIView,
@@ -16,19 +14,15 @@ from .serializers import (
 @require_safe
 def tag_list(request):
     tag_list = Tag.objects.all()
-    template = loader.get_template("tag/list.html")
     # tag_list is the varialbe referenced in the template
     context = {"tag_list": tag_list} 
-    html_content = template.render(context)
-    return HttpResponse(html_content)
+    return render(request, "tag/list.html", context)
 
 @require_safe
 def tag_detail(request, slug):
     tag = get_object_or_404(Tag, slug=slug)
-    template = loader.get_template("tag/detail.html")
     context = {"tag": tag}
-    html_content = template.render(context)
-    return HttpResponse(html_content)
+    return render(request, "tag/detail.html", context)
 
 class TagApiDetail(RetrieveAPIView):
 
