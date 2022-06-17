16b15d1506265044c9ea111a246b8535f91bcb46
Michael Bardouille <michael.bardouille@gmail.com>
2022-06-17 15:19:19 -0400

    chore/add Jupyter notebook

--- src/config/urls.py
+++ src/config/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from blog import urls as blog_urls
 from blog.routers import urlpatterns as blog_api_urls
-from organizer import urls as organizer_urls
 from organizer.routers import urlpatterns as organizer_api_urls
 
 api_urls = organizer_api_urls + blog_api_urls
