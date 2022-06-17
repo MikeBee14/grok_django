59575392530baeb24f3259938a1e86c3b929e9ef
Michael Bardouille <michael.bardouille@gmail.com>
2022-06-17 13:41:42 -0400

    chore/reconfigure config urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from blog import urls as blog_urls
+from blog.routers import urlpatterns as blog_api_urls
 from organizer import urls as organizer_urls
+from organizer.routers import urlpatterns as organizer_api_urls
+
+api_urls = organizer_api_urls + blog_api_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include(organizer_urls)),
-    path("", include(blog_urls)),
+    path("api/v1/", include(api_urls)),
 ]
