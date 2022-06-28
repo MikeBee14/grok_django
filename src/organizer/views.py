07f4c30f4fc1efb7c48279f6ac0f6d3dd7f7715b
Michael Bardouille <michael.bardouille@gmail.com>
2022-06-28 12:47:01 -0400

    chore/update fields through put

--- src/organizer/views.py
+++ src/organizer/views.py
@@ -2,18 +2,20 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 from rest_framework.generics import (
     ListAPIView,
+    ListCreateAPIView,
     RetrieveAPIView
 )
+from rest_framework.response import Response
+from rest_framework.status import(
+    HTTP_200_OK,
+    HTTP_400_BAD_REQUEST,
+)
+
 from .models import NewsLink, Startup, Tag
 from .serializers import (
     NewsLinkSerialzer,
     StartupSerializer, 
     TagSerializer
-)
-from rest_framework.response import Response
-from rest_framework.status import (
-    HTTP_201_CREATED,
-    HTTP_400_BAD_REQUEST
 )
 
 class StartupList(ListView):
@@ -46,24 +48,25 @@ class TagApiDetail(RetrieveAPIView):
     serializer_class = TagSerializer
     lookup_field = "slug"
 
+    def put(self, request, slug):
+        tag = get_object_or_404(Tag, slug=slug)
+        s_tag = TagSerializer(
+            tag,
+            data=request.data,
+            context={"request": request}
+        )
+        if s_tag.is_valid():
+            s_tag.save()
+            return Response(s_tag.data, status=HTTP_200_OK)
+        return Response(
+            s_tag.errors, status=HTTP_400_BAD_REQUEST
+        )
 
-class TagApiList(ListAPIView):
+class TagApiList(ListCreateAPIView):
 
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
-    def post(self, request):
-        s_tag = self.serializer_class(
-            data=request.data, context={"request": request}
-        )
-        if s_tag.is_valid():
-            s_tag.save()
-            return Response(
-                s_tag.data, status=HTTP_201_CREATED
-            )
-        return Response(
-            s_tag.errors, status=HTTP_400_BAD_REQUEST
-        )
 
 class StartupApiDetail(RetrieveAPIView):
 
