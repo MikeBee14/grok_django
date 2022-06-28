7d6ad3ac59b5f61f0a3ee42157956dcaf985bdc5
Michael Bardouille <michael.bardouille@gmail.com>
2022-06-28 15:33:52 -0400

    chore/using generic viewsets

--- src/organizer/routers.py
+++ src/organizer/routers.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
 from .views import (
     NewsLinkApiDetail,
@@ -10,25 +11,11 @@ from .views import (
 )
 from .viewsets import TagViewSet
 
-tag_create_list = TagViewSet.as_view(
-    {"get": "list", "post": "create"}
-)
+api_router = SimpleRouter()
+api_router.register("tag", TagViewSet, base_name="api-tag")
+api_routes = api_router.urls
 
-tag_retrieve_update_delete = TagViewSet.as_view(
-    {
-        "get": "retreive", 
-        "put": "update",
-        "patch": "partial_update", 
-        "delete": "delete"
-    }
-)
-
-urlpatterns = [
-    path("tag/", tag_create_list, name="api-tag-list"),
-    path(
-        "tag/<str:slug>/", 
-        tag_retrieve_update_delete, 
-        name="api-tag-detail"),
+urlpatterns = api_routes + [
     path("startup/", StartupApiList.as_view(), name="api-startup-list"),
     path(
         "startup/<str:slug>/", 
--- src/organizer/viewsets.py
+++ src/organizer/viewsets.py
@@ -1,66 +1,10 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.response import Response 
-from rest_framework.status import (
-    HTTP_201_CREATED,
-    HTTP_204_NO_CONTENT,
-    HTTP_400_BAD_REQUEST,
-)
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Tag
 from .serializers import TagSerializer
 
-class TagViewSet(ViewSet):
+class TagViewSet(ModelViewSet):
 
-    def list(self, request):
-        tag_list = Tag.objects.all()
-        s_tag = TagSerializer(
-            tag_list,
-            many=True,
-            context={"request": request},
-        )
-        return Response(s_tag.data)
-
-    def retrieve(self, request, slug):
-        tag = get_object_or_404(Tag, slug=slug)
-        s_tag = TagSerializer(
-            tag,
-            context={"request": request},
-        )
-        return Response(s_tag.data)
-
-    def create(self, request):
-        s_tag = TagSerializer(
-            data=request.data,
-            context={"request": request},
-        )
-        if s_tag.isvalid():
-            s_tag.save()
-        return Response(s_tag.data, status=HTTP_201_CREATED)
-
-    def update(self, request, slug):
-        tag = get_object_or_404(Tag, slug=slug)
-        s_tag = TagSerializer(
-            tag,
-            data=request.data,
-            context={"request": request},
-        )
-        if s_tag.isvalid():
-            s_tag.save()
-            return Response(s_tag.data)
-        return Response(s_tag.errors, status=HTTP_400_BAD_REQUEST)
-
-    def partial_update(self, request, slug):
-        tag = get_object_or_404(Tag, slug=slug)
-        s_tag = TagSerializer(
-            tag,
-            data=request.data,
-            partial=True,
-            context={"request": request},
-        )
-        if s_tag.isvalid():
-            s_tag.save()
-            return Response(s_tag.data)
-        return Response(s_tag.errors, status=HTTP_400_BAD_REQUEST)
-
-    
+    lookup_field = "slug"
+    queryset = Tag.objects.all()
+    serializer_class = Tag
