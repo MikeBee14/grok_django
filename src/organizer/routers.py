f94179d9280b740f9bc5fe4c66d7155057abf3b2
Michael Bardouille <michael.bardouille@gmail.com>
2022-06-28 13:27:24 -0400

    chore/wiring up viewsets

--- src/organizer/routers.py
+++ src/organizer/routers.py
@@ -8,12 +8,26 @@ from .views import (
     TagApiList, 
     TagApiDetail
 )
+from .viewsets import TagViewSet
+
+tag_create_list = TagViewSet.as_view(
+    {"get": "list", "post": "create"}
+)
+
+tag_retrieve_update_delete = TagViewSet.as_view(
+    {
+        "get": "retreive", 
+        "put": "update",
+        "patch": "partial_update", 
+        "delete": "delete"
+    }
+)
 
 urlpatterns = [
-    path("tag/", TagApiList.as_view(), name="api-tag-list"),
+    path("tag/", tag_create_list, name="api-tag-list"),
     path(
         "tag/<str:slug>/", 
-        TagApiDetail.as_view(), 
+        tag_retrieve_update_delete, 
         name="api-tag-detail"),
     path("startup/", StartupApiList.as_view(), name="api-startup-list"),
     path(
