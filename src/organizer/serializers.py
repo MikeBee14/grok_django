08057495eb2ad4bed11ba9d5730cd65f357b50d0
Michael Bardouille <michael.bardouille@gmail.com>
2022-06-28 16:40:32 -0400

    chore/refactor NewsLinkSerializer

--- src/organizer/serializers.py
+++ src/organizer/serializers.py
@@ -1,4 +1,11 @@
-from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer
+from rest_framework.relations import reverse
+from rest_framework.serializers import (
+    HyperlinkedModelSerializer, 
+    HyperlinkedRelatedField, 
+    ModelSerializer, 
+    SerializerMethodField,
+)
+
 from .models import NewsLink, Startup, Tag   
 
 class TagSerializer(ModelSerializer):
@@ -23,8 +30,23 @@ class StartupSerializer(HyperlinkedModelSerializer):
 
 class NewsLinkSerialzer(ModelSerializer):
 
-    startup = StartupSerializer
+    url = SerializerMethodField()
+    startup = HyperlinkedRelatedField(
+        queryset=Startup.objects.all(),
+        lookup_field="slug",
+        view_name="api-startup-detail"
+    )
 
     class Meta:
        model = NewsLink
-       fields = "__all__"
+       exclude = ("id",)
+    
+    def get_url(self, newslink):
+        return reverse(
+            "api-newslink-detail",
+            kwargs=dict(
+                startup_slug=newslink.startup.slug,
+                newslink_slug=newslink.slug
+            ),
+            request=self.context["request"]
+        )
